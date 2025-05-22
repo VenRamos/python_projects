5305d9dfb490ef04c6b89ccc5798d900dbdb9351
VenRamos <vfluis30@gmail.com>
2025-05-22 12:00:03 +0800

    Add files via upload

---
+++ 01 greetings_generator.py
@@ -0,0 +1,6 @@
+name = input('Let me know your name: ')
+age = input('Tell me your age: ')
+
+age_next_year = int(age) + 1
+
+print(f"Hello {name}! You are {age} years old, You will be {age_next_year} next year. Welcome!")
